3996350287c6c8d55dd66b238bea09a52b6a03c6
Mark Dickinson <marksamdickinson@gmail.com>
2020-06-01 14:16:44 -0700

    Reduce Telemetry Interface

---
+++ DickinsonBros.Telemetry.Abstractions/Models/TelemetryData.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DickinsonBros.Telemetry.Abstractions.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class TelemetryData
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int ElapsedMilliseconds { get; set; }
+        public int TelemetryState { get; set; }
+    }
+}
---
+++ DickinsonBros.Telemetry.Abstractions/Models/TelemetryType.cs
@@ -0,0 +1,10 @@
+﻿namespace DickinsonBros.Telemetry.Abstractions.Models
+{
+    public enum TelemetryType
+    {
+        API = 0,
+        Rest = 1,
+        SQL = 2,
+        Email = 3
+    }
+}
