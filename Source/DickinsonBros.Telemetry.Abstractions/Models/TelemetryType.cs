315671451a45fb09df138528b4bb090698f75f8e
Mark Dickinson <marksamdickinson@gmail.com>
2021-01-16 22:07:11 -0700

    Refactor

---
+++ Source/DickinsonBros.Telemetry.Abstractions/Models/TelemetryType.cs
@@ -0,0 +1,12 @@
+﻿namespace DickinsonBros.Telemetry.Abstractions.Models
+{
+    public enum TelemetryType
+    {
+        API         = 0,
+        Rest        = 1,
+        SQL         = 2,
+        Email       = 3,
+        Cosmos      = 4,
+        AzureTable  = 5
+    }
+}
